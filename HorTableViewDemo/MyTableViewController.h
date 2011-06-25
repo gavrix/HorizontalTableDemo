1cda33682c96b3f989c14ea379af8f5f59b3f41c
Sergey Gavrilyuk <sergey.gavrilyuk@gmail.com>
2011-06-26 02:35:48 +0300

    initial commit

---
+++ HorTableViewDemo/MyTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyTableViewController.h
+//  HorTableViewDemo
+//
+//  Created by Sergey Gavrilyuk on 6/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MyTableViewController : UITableViewController {
+    
+}
+
+@end
